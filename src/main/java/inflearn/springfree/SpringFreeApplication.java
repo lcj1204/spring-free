32f20be3b318dc0d824d32da0e13beb98aa25dc1
lcj1204 <dlckdwo1204@naver.com>
2023-01-13 17:25:36 +0900

    Initial commit

---
+++ src/main/java/inflearn/springfree/SpringFreeApplication.java
@@ -0,0 +1,13 @@
+package inflearn.springfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFreeApplication.class, args);
+	}
+
+}
